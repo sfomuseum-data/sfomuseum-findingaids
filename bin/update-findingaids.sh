db4a986562510dd3463b260511e86fbfd3822606
thisisaaronland <aaron@localhost>
2021-11-02 17:38:50 -0700

    make the update script work

--- bin/update-findingaids.sh
+++ bin/update-findingaids.sh
@@ -1,16 +1,53 @@
 #!/bin/sh
 
-SOURCES=`command -v wof-findingaid-sources`
-POPULATE=`command -v wof-findingaid-populate`
+# update-findingaids.sh -T 'awsparamstore://{PARAM_NAME}?region={REGION}&credentials=session'
+# update-findingaids.sh -T 'constant://?val={TOKEN}'
 
-GIT=`command -v git`
-DATE=`command -v date`
-BC=`command -v bc`
+SOURCES=`which wof-findingaid-sources`
+POPULATE=`which wof-findingaid-populate`
+RUNTIMEVAR=`which runtimevar`
+
+GIT=`which git`
+DATE=`which date`
+BC=`which bc`
 
 NOW=`${DATE} '+%s'`
 SINCE=$((${NOW} - 86400))	# 24 hours
 
-${GIT} clone https://github.com/sfomuseum-data/sfomuseum-findingaids.git /usr/local/data/sfomuseum-findingaid
+GITHUB_USER="sfomuseum-data"
+
+TOKEN_URI=""
+USAGE=""
+
+while getopts "T:h" opt; do
+    case "$opt" in
+        h) 
+	    USAGE=1
+	    ;;	
+	T)
+	    TOKEN_URI=$OPTARG
+	    ;;
+	:   )
+	    echo "WHAT WHAT WHAT"
+	    ;;
+    esac
+done
+
+if [ "${USAGE}" = "1" ]
+then
+    echo "usage: update.sh"
+    echo "options:"
+    echo "...please write me"
+    exit 0
+fi
+
+GITHUB_TOKEN=`${RUNTIMEVAR} "${TOKEN_URI}"`
+
+if [ "${GITHUB_TOKEN}" = "" ]
+then
+    echo "Missing GitHub access token"
+    exit 1
+fi
 
 REPOS=`${SOURCES} -provider-uri "github://sfomuseum-data?prefix=sfomuseum-data-&updated_since=${SINCE}"`
 
@@ -18,6 +55,8 @@ if [ "${REPOS}" = "" ]
 then
     exit
 fi
+
+${GIT} clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/sfomuseum-data/sfomuseum-findingaids.git /usr/local/data/sfomuseum-findingaid
 
 for REPO in ${REPOS}
 do
@@ -34,9 +73,17 @@ do
     time ${POPULATE} -iterator-uri git:///tmp -producer-uri ${PRODUCER_URI} ${REPO}
 done
 
+NAMES=""
+
+for REPO in ${REPOS}
+do
+    NAME=`basename ${REPO} | sed 's/\.git//g'`
+    NAMES="${NAMES} ${NAME}"
+done
+
 cd /usr/local/data/sfomuseum-findingaid
 git add data
-git commit -m "update finding aids for ${REPOS}"
+git commit -m "update finding aids for ${NAMES}"
 git push origin main
 
 
